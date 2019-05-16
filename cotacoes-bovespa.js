fda14e6f3e5ccd83da6e889a2eb7e15abde5331a
Charles <charles.moreira@sysmap.com.br>
2019-05-16 16:20:54 -0300

    Add gitigonere and fix issues

--- cotacoes-bovespa.js
+++ cotacoes-bovespa.js
@@ -13,15 +13,15 @@ module.exports.getCurrentQuote = function(ticker, callback){
 		var main = JSON.parse(body.split("root.App.main = ")[1].split(";\n}(this));")[0]);
 
 		var quote = {};
-		quote.currentPrice = parseFloat(main.context.dispatcher.stores.QuoteSummaryStore.financialData.currentPrice.fmt);
-		quote.regularMarketOpen = parseFloat(main.context.dispatcher.stores.QuoteSummaryStore.price.regularMarketOpen.fmt);
-		quote.regularMarketDayHigh = parseFloat(main.context.dispatcher.stores.QuoteSummaryStore.price.regularMarketDayHigh.fmt);
+		quote.price = parseFloat(main.context.dispatcher.stores.QuoteSummaryStore.financialData.currentPrice.fmt);
+		quote.open = parseFloat(main.context.dispatcher.stores.QuoteSummaryStore.price.regularMarketOpen.fmt);
+		quote.high = parseFloat(main.context.dispatcher.stores.QuoteSummaryStore.price.regularMarketDayHigh.fmt);
+		quote.low = parseFloat(main.context.dispatcher.stores.QuoteSummaryStore.price.regularMarketDayLow.fmt);
+		quote.previousClose = parseFloat(main.context.dispatcher.stores.QuoteSummaryStore.price.regularMarketPreviousClose.fmt);
+		quote.volume = parseFloat(main.context.dispatcher.stores.QuoteSummaryStore.price.regularMarketVolume.fmt);
+		quote.marketChange = parseFloat(main.context.dispatcher.stores.QuoteSummaryStore.price.regularMarketChange.fmt);
 		quote.shortName = main.context.dispatcher.stores.QuoteSummaryStore.price.shortName;
 		quote.longName = main.context.dispatcher.stores.QuoteSummaryStore.price.longName;
-		quote.regularMarketChange = parseFloat(main.context.dispatcher.stores.QuoteSummaryStore.price.regularMarketChange.fmt);
-		quote.regularMarketPreviousClose = parseFloat(main.context.dispatcher.stores.QuoteSummaryStore.price.regularMarketPreviousClose.fmt);
-		quote.regularMarketDayLow = parseFloat(main.context.dispatcher.stores.QuoteSummaryStore.price.regularMarketDayLow.fmt);
-		quote.regularMarketVolume = parseFloat(main.context.dispatcher.stores.QuoteSummaryStore.price.regularMarketVolume.fmt);
 
 		callback(null, quote);
 
